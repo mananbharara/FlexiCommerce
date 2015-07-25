1888dba4e2df2b67a4dc678e851b57990427d271
Manan <manan.bharara@gmail.com>
2015-07-25 13:09:43 +0200

    Working spec with parse mocks

--- cloud/controller/delivery-slot.js
+++ cloud/controller/delivery-slot.js
@@ -1,3 +1,11 @@
-exports.index = function () {
-  return [];
+var Parse = require('parse').Parse;
+
+var DeliverySlot = Parse.Object.extend('DeliverySlot');
+
+exports.index = function (req, res) {
+  var query = new Parse.Query(DeliverySlot);
+
+  query.find().then(function (results) {
+    res.send(results);
+  })
 };
---
+++ spec/controller/delivery-slot-spec.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var ParseMock = require('parse-mock');
+
+var deliverySlotController = require('../../cloud/controller/delivery-slot');
+
+describe('Delivery slot controller', function () {
+  it('create an instance of delivery slot', function () {
+    var req = {}, res = {};
+    res.send = sinon.spy();
+    ParseMock.stubQueryFind(function () {
+      return [];
+    });
+
+    deliverySlotController.index(req, res);
+
+
+    expect(res.send.calledOnce).to.equal(true);
+    expect(res.send.calledWith([])).to.equal(true);
+  });
+});

472f0ac03283eeeda25a69953d5e43325ee91f02
Manan <manan.bharara@gmail.com>
2015-07-25 16:54:39 +0200

    Adding routes

--- cloud/app.js
+++ cloud/app.js
@@ -8,5 +8,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 
 app.get('/delivery-slots', deliverySlotController.index);
+app.put('/delivery-slots/:id', deliverySlotController.decreaseAllowedDeliveries);
+app.put('/orders/delivery-slots', deliverySlotController.updateWithDeliverySlot);
 
 app.listen();
